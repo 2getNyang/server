8389c52092b7aab450723f9992135828a33a3aa2
dev-ssj <ju981010@naver.com>
2025-07-07 16:51:53 +0900

    chore : 프로젝트 초기 설정

---
+++ nyang/src/main/java/com/project/nyang/NyangApplication.java
@@ -0,0 +1,13 @@
+package com.project.nyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyangApplication.class, args);
+	}
+
+}
---
+++ nyang/src/test/java/com/project/nyang/NyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
