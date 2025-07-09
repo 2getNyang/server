8807c2fe4268160870d3e74c3e3ed0a4578a7c73
dev-ssj <ju981010@naver.com>
2025-07-09 22:34:46 +0900

    fix : BOARD, ANIMAL 엔티티 수정 및 공통응답 API 수정

--- nyang/src/main/java/com/project/nyang/global/common/api/ApiErrorResponse.java
+++ nyang/src/main/java/com/project/nyang/global/common/api/ApiErrorResponse.java
@@ -6,32 +6,27 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-/**
- * Error관련 API 공통응답 클래스입니다.
- *
- * @author : 선순주
- * @fileName : ApiErrorResponse
- * @since : 2025-07-08
- */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Schema(description = "공통 에러 응답 포맷")
 public class ApiErrorResponse {
+
     @Schema(description = "에러 코드", example = "401")
     private int code;
+
     @Schema(description = "에러 메시지", example = "비밀번호가 맞지 않습니다")
     private String message;
+
     @Schema(description = "에러 상세 메시지", example = "CustomException: 비밀번호가 맞지 않습니다")
     private String detail;
 
-    //예외 응답 :  code, data, message
-    public static <T> ApiResponse<T> error(int code, String message) {
-        return ApiResponse.<T>builder()
+    public static ApiErrorResponse of(int code, String message, String detail) {
+        return ApiErrorResponse.builder()
                 .code(code)
-                .data(null)
                 .message(message)
+                .detail(detail)
                 .build();
     }
 }
