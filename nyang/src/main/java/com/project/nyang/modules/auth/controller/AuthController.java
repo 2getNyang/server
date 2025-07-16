45eeef980e6b87938738bb8cac3f0ab594ef5513
Ljeelra <dlwldms9803@gmail.com>
2025-07-16 19:06:14 +0900

    feat : 구글계정 연동해제 주석삭제 #18

--- nyang/src/main/java/com/project/nyang/modules/auth/controller/AuthController.java
+++ nyang/src/main/java/com/project/nyang/modules/auth/controller/AuthController.java
@@ -3,26 +3,21 @@ package com.project.nyang.modules.auth.controller;
 import com.project.nyang.global.common.api.ApiSuccessResponse;
 import com.project.nyang.global.exception.CustomException;
 import com.project.nyang.global.exception.ErrorCode;
-//import com.project.nyang.global.security.oauth2.OAuth2UnlinkService;
 import com.project.nyang.modules.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -107,59 +102,33 @@ public class AuthController {
 //        return ResponseEntity.ok().build();
 //    }
 
-//    @Operation(summary = "로그아웃", description = "JWT(accessToken) 쿠키를 삭제해 로그아웃합니다")
-//    //보통 로그아웃은 쿠키삭제로 처리한다.
-//    @PostMapping("/logout")
-//    public ResponseEntity<?> logout(HttpServletResponse response) {
-//
-//        // accessToken 쿠키 삭제
-//        Cookie accessTokenCookie = new Cookie("accessToken", null);
-//        accessTokenCookie.setHttpOnly(true);
-//        accessTokenCookie.setPath("/");
-//        accessTokenCookie.setMaxAge(0); // 즉시 만료!
-//Withdrawal of membership
-//        // refreshToken 쿠키 삭제
-//        Cookie refreshTokenCookie = new Cookie("refreshToken", null);
-//        refreshTokenCookie.setHttpOnly(true);
-//        refreshTokenCookie.setPath("/");
-//        refreshTokenCookie.setMaxAge(0);
-//
-//        // 응답에 쿠키 삭제 포함
-//        response.addCookie(accessTokenCookie);
-//        response.addCookie(refreshTokenCookie);
-//
-//        // (추가) 서버 세션도 있다면 만료
-//        // request.getSession().invalidate();
-//
-//        return ResponseEntity.ok().body("로그아웃 완료 (쿠키 삭제됨)");
-//    }
 
-//    @Operation(
-//            summary = "구글 연동 해제 (토큰 폐기)",
-//            description = """
-//                    구글 소셜 로그인 연동 해제(토큰 폐기)를 수행합니다.
-//                    연동 해제 후 사용자는 동일 구글 계정으로 재로그인 시 신규 회원으로 가입 처리됩니다.
-//                    """,
-//            responses = {
-//                    @ApiResponse(responseCode = "200", description = "연동 해제(토큰 폐기) 성공"),
-//                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
-//                    @ApiResponse(responseCode = "500", description = "서버 오류")
-//            }
-//    )
-//    @PostMapping("/revoke")
-//    public ResponseEntity<?> revokeGoogle(@RequestHeader("Authorization") String authorizationHeader){
-//        //Todo. DB에서 회원삭제처리에 대한 timestamp 처리는 있던데 해당 계정의 상태 컬럼은 따로 없는 것 같다. 07-14 회의후 User테이블 수정해야할 것 같다.
-//        String accessToken = authorizationHeader.replace("Bearer ", "");
-//        boolean success = authService.revokeGoogleAccessToken(accessToken);
-//
-//        if (success) {
-//            //Todo. 아직 회원 탈퇴 기능 작성 전이라 주석처리 해둠. 현재 계정연동끊는 기능만 하는중
-//            //userService.markUserWithdrawnByAccessToken(accessToken);
-//            return ResponseEntity.ok("구글 연결 해제 완료");
-//        } else {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("구글 연결 해제 실패");
-//        }
-//
-//    }
+    @Operation(
+            summary = "구글 연동 해제 (토큰 폐기)",
+            description = """
+                    구글 소셜 로그인 연동 해제(토큰 폐기)를 수행합니다.
+                    연동 해제 후 사용자는 동일 구글 계정으로 재로그인 시 신규 회원으로 가입 처리됩니다.
+                    """,
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "연동 해제(토큰 폐기) 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "500", description = "서버 오류")
+            }
+    )
+    @PostMapping("/revoke")
+    public ResponseEntity<?> revokeGoogle(@RequestHeader("Authorization") String authorizationHeader){
+        //Todo. DB에서 회원삭제처리에 대한 timestamp 처리는 있던데 해당 계정의 상태 컬럼은 따로 없는 것 같다. 07-14 회의후 User테이블 수정해야할 것 같다.
+        String accessToken = authorizationHeader.replace("Bearer ", "");
+        boolean success = authService.revokeGoogleAccessToken(accessToken);
+
+        if (success) {
+            //Todo. 아직 회원 탈퇴 기능 작성 전이라 주석처리 해둠. 현재 계정연동끊는 기능만 하는중
+            //userService.markUserWithdrawnByAccessToken(accessToken);
+            return ResponseEntity.ok("구글 연결 해제 완료");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("구글 연결 해제 실패");
+        }
+
+    }
 
 }
